6517d9029f259496c5043d5f4b7564e55f32af36
saurabhHD <saurabhinpurani@gmail.com>
2024-03-28 18:20:48 +0530

    added description of homepage, remove buttons

--- src/components/contact-us/index.jsx
+++ src/components/contact-us/index.jsx
@@ -5,12 +5,12 @@ import {PhoneOutlined, MailOutlined} from "@ant-design/icons"
 
 const ContactUs = ()=> {
     return (
-        <div className="pb-3 mt-5 bg-[url('/images/map-blue.jpg')] bg-cover h-screen">
+        <div className="pb-3 mt-5 md:bg-[url('https://res.cloudinary.com/dhrzyzezw/image/upload/f_auto,q_auto/v1/portfolio/ao9jv6pcgcdzqmoqv7gk')] bg-cover h-screen">
             <div className="text-center bg-white mt-3 py-3">
                 <span className="text-xl font-bold p-2 hover:bg-[#FCC255] hover:text-white">Contact Me</span>
                 <h1 className="text-3xl font-bold mt-3 text-[#556D91]">Get In Touch</h1>
             </div>
-            <div className="flex justify-center  items-center md:px-10 px-3 mt-6 py-5 "> 
+            <div className="md:flex justify-center  items-center md:px-10 px-3 mt-6 py-5 "> 
                 {/* <div>
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d56048.43037220901!2d77.17435457408114!3d28.598969862942468!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cfd5b347eb62d%3A0x37205b715389640!2sDelhi!5e0!3m2!1sen!2sin!4v1710760697993!5m2!1sen!2sin"  loading="lazy" className="w-[100%] h-[100%]">
 
--- src/components/portfolio/index.jsx
+++ src/components/portfolio/index.jsx
@@ -1,25 +1,32 @@
 const Portfolio = ()=> {
-
-
-    return (
+   return (
         <div className="py-3">
             <div className="text-center">
                 <span className="text-xl font-bold p-2 hover:bg-[#FCC255] hover:text-white">Quality Work</span>
                 <h1 className="text-3xl font-bold mt-3 text-[#556D91]">My Projects</h1>
             </div>
-            <div className="flex justify-center">
-                {/* <div className="grid grid-cols-3 gap-5">
+                <div className="grid grid-cols-3 gap-5 m-8">
                     <div className="shadow-lg rounded">
-                        <img src="https://res.cloudinary.com/dhrzyzezw/image/upload/f_auto,q_auto/v1/portfolio/drdcbpedjakeiuhvkx6u" alt="photosignatureresize" className="w-100 h-3"/>
+                        <img src="https://res.cloudinary.com/dhrzyzezw/image/upload/f_auto,q_auto/v1/portfolio/drdcbpedjakeiuhvkx6u" alt="photosignatureresize" className="w-full h-80 object-cover"/>
                     </div>
                     <div className="shadow-lg rounded">
-                        <img src="https://res.cloudinary.com/dhrzyzezw/image/upload/f_auto,q_auto/v1/portfolio/drdcbpedjakeiuhvkx6u" alt="photosignatureresize" className="h-full"/>
+                        <img src="https://res.cloudinary.com/dhrzyzezw/image/upload/f_auto,q_auto/v1/portfolio/drdcbpedjakeiuhvkx6u" alt="photosignatureresize" className="w-full h-80 object-cover"/>
                     </div>
                     <div className="shadow-lg rounded">
-                        <img src="https://res.cloudinary.com/dhrzyzezw/image/upload/f_auto,q_auto/v1/portfolio/drdcbpedjakeiuhvkx6u" alt="photosignatureresize" className="h-full"/>
+                        <img src="https://res.cloudinary.com/dhrzyzezw/image/upload/f_auto,q_auto/v1/portfolio/drdcbpedjakeiuhvkx6u" alt="photosignatureresize" className="w-full h-80 object-cover"/>
                     </div>
-                </div> */}
-            </div>
+                </div>
+                <div className="grid grid-cols-3 gap-5 m-8">
+                    <div className="shadow-lg rounded">
+                        <img src="https://res.cloudinary.com/dhrzyzezw/image/upload/f_auto,q_auto/v1/portfolio/drdcbpedjakeiuhvkx6u" alt="photosignatureresize" className="w-full h-80 object-cover"/>
+                    </div>
+                    <div className="shadow-lg rounded">
+                        <img src="https://res.cloudinary.com/dhrzyzezw/image/upload/f_auto,q_auto/v1/portfolio/drdcbpedjakeiuhvkx6u" alt="photosignatureresize" className="w-full h-80 object-cover"/>
+                    </div>
+                    <div className="shadow-lg rounded">
+                        <img src="https://res.cloudinary.com/dhrzyzezw/image/upload/f_auto,q_auto/v1/portfolio/drdcbpedjakeiuhvkx6u" alt="photosignatureresize" className="w-full h-80 object-cover"/>
+                    </div>
+                </div>
         </div>
     )
 }
